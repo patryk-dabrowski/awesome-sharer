cc2902f46dce9572cdaffb286f2d18f430b88020
Patryk Dabrowski <patrykdabrowski58@gmail.com>
2021-02-06 18:57:53 +0100

    Redirect to login page if unauthorized

--- url/urls.py
+++ url/urls.py
@@ -1,6 +1,8 @@
+from django.contrib.auth.decorators import login_required
 from django.urls import path
-from django.views.generic import TemplateView
+
+from url.views import HomeView
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('', login_required(HomeView.as_view()), name='home'),
 ]
